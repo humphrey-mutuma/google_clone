8f063f82044bdc67b61ed8915de9150199f8cfc9
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-05-06 16:30:34 +0300

    complete styling of search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,6 +2,7 @@ import './Search.css'
 
 import SearchIcon from '@material-ui/icons/Search';
 import MicIcon from "@material-ui/icons/Mic";
+import { Button } from '@material-ui/core';
 
 const Search = () => {
   return (
@@ -11,8 +12,12 @@ const Search = () => {
         <input />
         <MicIcon />
       </div>
+      <div className="search_buttons">
+        <Button onClick={search} variant="outlined">Google Search</Button>
+        <Button variant="outlined">I'm Feeling Licky</Button>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Search
