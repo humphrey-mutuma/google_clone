f1505b76ade54bf33c7d69bd50044d509f54c7fd
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-05-06 12:54:36 +0300

    Add Router & material-ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Home from './pages/Home';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app">
+      <Route>
+         <h1>yoo</h1>
+        <Home />
+      </Route>
+     
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>This is the homepage</h1>
+      <div className="home_header">
+        <div className="home_headerLeft">
+          {/* link */}
+          {/* link */}
+        </div>
+        <div className="home_headerRight">
+          {/* link */}
+          {/* link */}
+          {/* link */}
+
+        </div>
+      </div>
+      <div className="home_body">
+
+      </div>
+    </div>
+  )
+}
+
+export default Home
